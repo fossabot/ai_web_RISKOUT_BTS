645b28e7c2d3686c1dfad5f0f68cfdaa167b02a1
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-09-14 14:38:09 +0000

    create django superuser

---
+++ WEB/backend/django/riskout/riskout/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for riskout project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'riskout.settings')
+
+application = get_wsgi_application()
