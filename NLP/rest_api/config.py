de03eb5de09b4d58cbfc1e709b8740d1ae38ad64
PFF <50140505+playff@users.noreply.github.com>
2021-09-15 08:06:03 +0000

    add model path

--- NLP/rest_api/config.py
+++ NLP/rest_api/config.py
@@ -4,5 +4,6 @@ FILE_UPLOAD_PATH = os.getenv("FILE_UPLOAD_PATH", "./file_upload")
 
 LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 ROOT_PATH = os.getenv("ROOT_PATH", "/")
+MODEL_PATH = os.getenv("MODEL_PATH", "/home/user/models")
 
 CONCURRENT_REQUEST_PER_WORKER = int(os.getenv("CONCURRENT_REQUEST_PER_WORKER", 4))
