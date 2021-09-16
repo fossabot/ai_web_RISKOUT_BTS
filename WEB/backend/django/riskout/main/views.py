b1038b08171d8b475588fc1322d4f5ad6547b308
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-09-16 10:56:58 +0000

    initial commit

--- WEB/backend/django/riskout/main/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def main_html(requests):
-    return render(requests, 'index.html')
--- WEB/backend/django/riskout/user/serializers.py
+++
@@ -1,6 +0,0 @@
-from .models import CustomUser
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = CustomUser
-        fields = ('email', 'last_login', 'date_joined', 'is_staff')
