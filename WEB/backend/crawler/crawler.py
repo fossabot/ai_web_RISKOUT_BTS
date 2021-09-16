24aee0149a18308811b7da9cf32d29f49b28bc29
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-09-16 07:22:57 +0000

    tide up codes and clear TODOs now use dict to return results

--- WEB/backend/crawler/crawler.py
+++ WEB/backend/crawler/crawler.py
@@ -1,12 +1,14 @@
 import requests
 from bs4 import BeautifulSoup as bs
 
+# 앞으로는 date와 page를 for 문으로 돌면서 검색하는 함수 만들면 될듯
+TARGET_URL = "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid2=268&sid1=100&date=20210916&page=1"
 custom_header = {
     'referer' : "https://www.naver.com/",
     'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
 }
 
-def crawling(soup):
+def scrap_body_contents(soup):
     div = soup.find("div", class_="_article_body_contents")
     
     result = div.get_text()
@@ -14,20 +16,22 @@ def crawling(soup):
     # print(result)
     return result
 
-def get_href(soup):
-    href = []
-    img = []
-    title = []
+def get_contents(soup):
+    ret = []
 
     div = soup.find("div", class_ = 'list_body newsflash_body')
 
-    
     for dt in div.find_all("dt", class_="photo"):
-        href.append(dt.find('a')['href'])
-        img.append(dt.find('img')['src'])
-        title.append(dt.find('img')['alt']) # 일단은 테스트로 제목을 img의 alt로 가져왔지만 수정이 필요함. crawling함수 안에서 처리할 수도 있을 듯
+        temp_dict = {}
+        href = dt.find('a')['href']
+        temp_dict['href'] = href
+        temp_dict['img'] = dt.find('img')['src']
+        temp_dict['title'] = dt.find('img')['alt'] # 일단은 테스트로 제목을 img의 alt로 가져왔지만 수정이 필요함. crawling함수 안에서 처리할 수도 있을 듯
+        temp_dict['body_contents'] = str.strip(scrap_body_contents(bs(requests.get(href, headers = custom_header).text, "html.parser")))
 
-    return zip(href, img, title)
+        ret.append(temp_dict)
+
+    return ret
 
 def get_request(url):
     response = requests.get(url, headers = custom_header)
@@ -36,31 +40,15 @@ def get_request(url):
 
 
 def main():
-    url = "https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid1=100&sid2=268"
-    response = get_request(url)
+    response = get_request(TARGET_URL)
 
-    html= response.text
-    soup = bs(html, 'html.parser')
+    newslist_html = response.text
+    newslist_soup = bs(newslist_html, 'html.parser')
 
-    href_img_list = get_href(soup)
-
-    # print(href_img_list)
-
-    result = []
-
-    for href, img, title in href_img_list:
-        href_req = requests.get(href, headers = custom_header)
-        href_soup = bs(href_req.text, "html.parser")
-        temp = []
-        temp.append(str.strip(crawling(href_soup)))
-        
-        temp.append(img)
-        temp.append(title)
-        temp.append(href)
-        result.append(temp)
+    news_contents = get_contents(newslist_soup)
     
-    for tmp in result:
-        print("title: " + tmp[2] + "\nhref: " + tmp[3] + "\nimage: " + tmp[1] + '\ncontent:\n' + tmp[0] + '\n\n')
+    for tmp in news_contents:
+        print("title: " + tmp['title'] + "\nhref: " + tmp['href'] + "\nimage: " + tmp['img'] + '\ncontent:\n' + tmp['body_contents'] + '\n\n')
 
 if __name__ == '__main__':
     main()
