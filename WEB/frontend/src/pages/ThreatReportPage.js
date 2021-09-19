2d7799ac7ce2bb89771b8185b15966a12676c03c
Simon Seo <simonseo.doubles@gmail.com>
2021-09-19 14:47:28 +0000

    add example component

--- WEB/frontend/src/pages/ThreatReportPage.js
+++ WEB/frontend/src/pages/ThreatReportPage.js
@@ -5,24 +5,14 @@ import icon01 from '../images/sub/btn_icon01.png';
 import icon02 from '../images/sub/btn_icon02.png';
 import icon03 from '../images/sub/btn_icon03.png';
 import Layout from '../layout';
+import Sidebar from '../components/Sidebar';
 
 function App() {
 
 	return (
 		<Layout>
+			<Sidebar />
 			<div id="wrap">
-				<header id="sub_header">
-					<h1><a href="/"><img src="../images/sub/logo_w.png" alt="" /></a></h1>
-					<button className="prev_btn"><img src="../images/sub/prev_btn.png" alt="" /></button>
-					<ul className="sub_menu">
-						<li className="on"><a href="/">위협보고</a></li>
-						<li><a href="/">기밀 유출 현황</a></li>
-						<li><a href="/">허위 정보 검사</a></li>
-						<li><a href="/">로그아웃</a></li>
-					</ul>
-					<p className="copyright">Copyright © 2021. RISKOUT All right reserved.</p>
-				</header>
-
 				<section id="sub_contents">
 					<div className="sub01_wrap">
 						<h2 className="h2_tit">Welcome to Risk Out, 민석.</h2>
