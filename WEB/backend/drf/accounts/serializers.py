52726a0ebbe71a0a46aab121495be7fe0b811b62
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-09-19 15:12:57 +0900

    User authentication - Withdrawal (#28)
    * merge gitignore
    
    * delete API_docs.json
    
    * API docs는 나중에 따로 마크다운으로 만들 예정이라 삭제했습니다.
    
    * delete duplicated line
    
    * Complete member withdrawal function

--- WEB/backend/drf/accounts/serializers.py
+++ WEB/backend/drf/accounts/serializers.py
@@ -21,12 +21,8 @@ class RegisterSerializer(serializers.ModelSerializer):
         return user
 
 # Change Password
-from rest_framework import serializers
-from django.contrib.auth.models import User
-
 class ChangePasswordSerializer(serializers.Serializer):
     model = User
 
     old_password = serializers.CharField(required=True)
     new_password = serializers.CharField(required=True)
-    
