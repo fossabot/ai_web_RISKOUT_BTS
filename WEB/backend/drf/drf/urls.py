a23fe1c4ac66a75c8a02a2c0b76ddff5b3d2992d
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-09-19 08:07:33 +0000

    move password_reset path
    * /drf/urls.py -> /drf/accounts/urls.py

--- WEB/backend/drf/drf/urls.py
+++ WEB/backend/drf/drf/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('accounts.urls')),
-    path('api/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
 ]
