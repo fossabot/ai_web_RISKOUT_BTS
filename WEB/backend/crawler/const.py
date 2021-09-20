c838bd0b12909e1f811f182e476e41f05d73d796
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-09-20 07:17:31 +0000

    add crawing interval time

--- WEB/backend/crawler/const.py
+++ WEB/backend/crawler/const.py
@@ -3,3 +3,4 @@ NAVER_CUSTOM_HEADER = {
     'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
 }
 
+CRAWLING_INTERVAL = 2
