222e0e6ea04d0fc171e7bef989e7b82aa3da82c6
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-09-20 06:34:44 +0000

    리스트 페이지를 크롤링 하는 기능 추가

--- WEB/backend/crawler/crawler.py
+++ WEB/backend/crawler/crawler.py
@@ -9,43 +9,23 @@ import aiohttp
 # for checking elapesed time
 import time
 
-# for build-up naver news url
-from datetime import datetime, timedelta
+from config import jsonServer as js
 
-from model import Content, ListPage, NewsPage
-from const import *
-from db import DB
-
-def get_news_urls(soup):
-    """
-    page 안에서 뉴스들의 url을 찾아 리스트 형태로 리턴하는 함수
-    """
-    ret = []
-    div = soup.find("div", class_ = 'list_body newsflash_body')
-
-    for dt in div.find_all("dt", class_="photo"):
-        href = dt.find('a')['href']
-        ret.append(href)
-
-    return ret
-
-def get_listamount(soup):
-    """
-    first page안에서 페이지 목록 개수를 리턴하는 함수
-    """
-    div = soup.find("div", class_="paging")
-    hrefs = div.find_all('a')
-    return len(hrefs)
+from model import Content as ct
+from model import NaverListPage as nlp
+from model import NaverNewsPage as nnp
+import const as const
+import db as database
 
 def get_request(url):
     """
     url에서 response 받아 리턴하는 간단한 함수
     """
-    response = requests.get(url, headers = CUSTOM_HEADER)
+    response = requests.get(url, headers = const.NAVER_CUSTOM_HEADER)
 
     return response
 
-async def get_contents(news_url, db):
+async def get_contents(news_url, news_page, db):
     """
     news_url에서 contents 객체를 만들어 리턴하는 함수
     페이지 각각에서 스크래핑하는 기능을 담당하고 있다
@@ -54,42 +34,58 @@ async def get_contents(news_url, db):
 
     # aiohttp 이용
     async with aiohttp.ClientSession() as sess:
-        async with sess.get(news_url, headers = CUSTOM_HEADER) as res:
+        async with sess.get(news_url, headers = const.NAVER_CUSTOM_HEADER) as res:
             text = await res.text()
             content_soup = bs(text, 'html.parser')
-            news_content = contents_factory(news_url, content_soup)
+            news_content = ct.contents_factory(news_url, content_soup, news_page)
             # TODO news_content를 쿼리로 쏘는 코드 작성
-            db.put_content(news_content)
+            # db.put_content(news_content)
 
     print(f"Received request to {news_url}")
 
 async def main():
-    db = DB()
+    db = database.DB()
 
-    date = datetime.today()
-    date_format = date.strftime("%Y%m%d")
-    # 맨 뒤에 페이지 번호 숫자(1~9999등)을 붙여 페이지를 이동하기 위함.
-    eachday_urlbases = [NAVER_BASE] * CRAWL_DATEAMOUNT
-    
-    for i in range(CRAWL_DATEAMOUNT):
-        eachday_urlbases[i] += f"&date={date_format}&page="
-        date -= timedelta(days=1)
-        date_format = date.strftime("%Y%m%d")
+    # NaverListPage 객체 생성 및 기본 URL 얻기
+    naver_list_page = nlp.NaverListPage(js.get_naverlist())
+    eachday_urlbases = naver_list_page.get_eachday_urlbases()
+
+    # NaverNewsPage 객체 생성
+    naver_news_page = nnp.NaverNewsPage(js.get_navernews())
     
     for urlbase in eachday_urlbases:
         print("urlbase: " + urlbase)
 
-        for i in range(1, MAX_LISTPAGE_CRAWL): # TODO 페이지의 끝까지 다다랐을 때 탈출할 수 있도록 조정해야 함
-            response = get_request(urlbase + str(i))
+        prev_page = 0
+        now_page = 1
+        # for i in range(1, MAX_LISTPAGE_CRAWL):
+        # 아직 now_page를 읽어오는 함수가 검증되지 않았으므로, 테스트를 끝내기 위한 조건 추가
+        test_breaker = 0
+        while prev_page != now_page or test_breaker < 5:
+            response = get_request(urlbase + str(now_page))
+            print('\nlisturl: ' + urlbase + str(now_page) + '\n')
 
             newslist_html = response.text
             newslist_soup = bs(newslist_html, 'html.parser')
 
-            news_urls= get_news_urls(newslist_soup)
+            now_page = naver_list_page.get_nowpage(newslist_soup)
+            # 일단 마음에 안 들지만 이렇게 해 두었습니다.
+            if(now_page == prev_page):
+                break
 
-            futures = [asyncio.ensure_future(get_contents(news_url, db)) for news_url in news_urls]
+            news_urls= naver_list_page.get_news_urls(newslist_soup)
+
+            futures = [asyncio.ensure_future(get_contents(news_url, naver_news_page, db)) for news_url in news_urls]
 
             await asyncio.gather(*futures)
+
+            print("nowpage: " + str(now_page) + '\n')
+            time.sleep(2)
+
+            test_breaker += 1
+
+            prev_page = now_page
+            now_page += 1
 
 
 if __name__ == '__main__':
