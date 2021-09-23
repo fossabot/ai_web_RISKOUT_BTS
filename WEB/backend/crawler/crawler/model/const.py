ba0a766bc5532b463f52256656b29b8a921f046b
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-09-23 14:26:08 +0000

    네이버 관련 const 정리

--- WEB/backend/crawler/crawler/model/const.py
+++ WEB/backend/crawler/crawler/model/const.py
@@ -1,9 +1,3 @@
-#네이버 뉴스 하드코딩
-POLITICS = 100
-NORTH_KOREA = 268
-
-NAVER_BASE = f"https://news.naver.com/main/list.naver?mode=LS2D&mid=shm&sid2={NORTH_KOREA}&sid1={POLITICS}"
-
 
 # 크롤링할 날짜 수
 CRAWL_DATEAMOUNT = 1
