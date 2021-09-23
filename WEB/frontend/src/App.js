cab7ccbc3c9375ec024002a9a198bcd46c99827e
Simon Seo <simonseo.doubles@gmail.com>
2021-09-23 14:14:14 +0000

    simplify routing and sidebar

--- WEB/frontend/src/App.js
+++ WEB/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Modal/Header';
 import LoginModal from './components/Modal/LoginModal';
 import InitInfo from './components/Modal/InitInfo';
@@ -15,32 +15,32 @@ function App() {
   const [user, setUser] = useState([])
 
   let [isAuthenticated, setisAuthenticated] = useState(localStorage.getItem('token') ? true : false)
-  
-  const userHasAuthenticated = (authenticated, username, token) => { 
+
+  const userHasAuthenticated = (authenticated, username, token) => {
     setisAuthenticated(authenticated)
     setUser(username)
     localStorage.setItem('token', token);
   }//회원가입이나 로그인이 성공했을 때 토큰을 저장
 
   const handleLogout = () => {
-      setisAuthenticated(false)
-      setUser("")
-      localStorage.removeItem('token');
-      setModal(false)
+    setisAuthenticated(false)
+    setUser("")
+    localStorage.removeItem('token');
+    setModal(false)
   }//로그아웃
 
   //회원가입이나 로그인이 성공했을 때 modal을 변경해 로그인 버튼을 없애고 정보 수정과 회원 탈퇴 버튼 나오게하는 setModal
   //useEffect의 두번째 인자는 모든 렌더링 후 두번째 인자가 변경될때에만 실행되라는 내용 
-  useEffect(()=>{
-    if(isAuthenticated){
+  useEffect(() => {
+    if (isAuthenticated) {
       setModal(true)
     }
-    else{
+    else {
       setModal(false)
     }
-  },[isAuthenticated])
-  
-  
+  }, [isAuthenticated])
+
+
   useEffect(() => {
     // 토큰(access token)이 이미 존재하는 상황이라면 서버에 GET /validate 요청하여 해당 access token이 유효한지 확인
     if (isAuthenticated) {
@@ -51,88 +51,83 @@ function App() {
           Authorization: `JWT ${localStorage.getItem('token')}`
         }
       })
-      .then(res => {
-        fetch('http://localhost:8000/user/current/', {
-          headers: {
-            Authorization: `JWT ${localStorage.getItem('token')}`
-          }
-        })
-        .then(res => res.json())
-        .then(json => {
-          // 현재 유저 정보 받아왔다면, 로그인 상태로 state 업데이트 하고
-          if (json.username) {
-            setUser(json.username);
-          }else{
-            //유저가 undefined라면 로그인버튼이 나오도록 modal을 false로 항상 맞춰줌
-            setModal(false)
-            setisAuthenticated(false)
-          }
-          // Refresh Token 발급 받아 token의 만료 시간 연장
-          fetch('http://localhost:8000/refresh/', {
-            method: 'POST',
+        .then(res => {
+          fetch('http://localhost:8000/user/current/', {
             headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-              token: localStorage.getItem('token')
+              Authorization: `JWT ${localStorage.getItem('token')}`
+            }
+          })
+            .then(res => res.json())
+            .then(json => {
+              // 현재 유저 정보 받아왔다면, 로그인 상태로 state 업데이트 하고
+              if (json.username) {
+                setUser(json.username);
+              } else {
+                //유저가 undefined라면 로그인버튼이 나오도록 modal을 false로 항상 맞춰줌
+                setModal(false)
+                setisAuthenticated(false)
+              }
+              // Refresh Token 발급 받아 token의 만료 시간 연장
+              fetch('http://localhost:8000/refresh/', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                  token: localStorage.getItem('token')
+                })
+              })
+                .then(res => res.json())
+                .then((json) => {
+                  userHasAuthenticated(true, json.user.username, json.token);
+                })
+                .catch(error => {
+                  console.log(error);
+                });;
             })
-          })
-          .then(res => res.json())
-          .then((json)=>{
-            userHasAuthenticated(true, json.user.username, json.token);
-          })
-          .catch(error => {
-            console.log(error);
-          });;
+            .catch(error => {
+              handleLogout();
+              console.log(error)
+            });
         })
         .catch(error => {
           handleLogout();
           console.log(error)
         });
-      })
-      .catch(error => {
-        handleLogout();
-        console.log(error)
-      });
     }
-  },[isAuthenticated])
+  }, [isAuthenticated])
 
   return (
     <>
       <div className="App">
         <div className="auto-margin">
+          <Header modal={modal} handleLogout={handleLogout} />
           <Route exact path="/">
-            <Header modal={modal} handleLogout={handleLogout}/>
-            <Board/>
+            <Board />
           </Route>
 
           <Route exact path="/riskreport">
-            <Header modal={modal} handleLogout={handleLogout}/>
-            <RiskReport/>
+            <RiskReport />
           </Route>
 
           <Route exact path="/secret">
-            <Header modal={modal} handleLogout={handleLogout}/>
-            <Secret/>
+            <Secret />
           </Route>
 
           <Route exact path="/fakenews">
-            <Header modal={modal} handleLogout={handleLogout}/>
-            <FakeNews/>
+            <FakeNews />
           </Route>
 
           <Route exact path="/login">
-            <Header modal={modal} handleLogout={handleLogout}/>
-            <LoginModal setModal={setModal} userHasAuthenticated={userHasAuthenticated}/>
+            <LoginModal setModal={setModal} userHasAuthenticated={userHasAuthenticated} />
           </Route>
 
           <Route exact path="/init">
-          <Header modal={modal} handleLogout={handleLogout}/>
-            <InitInfo/>
+            <InitInfo />
           </Route>
 
+        </div>
       </div>
-    </div>
     </>
   );
 }
