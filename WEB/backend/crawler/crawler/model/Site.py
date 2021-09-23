ffdf548f5208fa98f5b945e6679aea6021582571
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-09-23 14:51:23 +0000

    Site에 옮길 수 있는 코드들 중복 방지를 위해 정리하여 옮김

--- WEB/backend/crawler/crawler/model/Site.py
+++ WEB/backend/crawler/crawler/model/Site.py
@@ -24,11 +24,68 @@ class Site:
         self.listpage = listpage(listjson)
         self.contentspage = contentspage(contentjson)
 
-    def get_request(self, url):
-        pass
+    def get_request(self, url, header):
+        """
+        url에서 response 받아 리턴하는 간단한 함수
+        """
+        response = requests.get(url, headers = header)
 
-    async def get_contents(self, news_url, news_page, db):
-        pass
+        return response
 
-    def crawl(self):
-        pass
+    async def get_contents(self, news_url, news_page, db, header):
+        """
+        news_url에서 contents 객체를 만들어 리턴하는 함수
+        페이지 각각에서 스크래핑하는 기능을 담당하고 있다
+        """
+        print(f"Send request to {news_url}")
+
+        # aiohttp 이용
+        async with aiohttp.ClientSession() as sess:
+            async with sess.get(news_url, headers = header) as res:
+                text = await res.text()
+                content_soup = bs(text, 'html.parser')
+                news_content = Content.contents_factory(news_url, content_soup, news_page)
+                # news_content를 쿼리로 쏘는 코드
+                db.put_content(news_content)
+
+        print(f"Received request to {news_url}")
+
+    async def crawl(self, header):
+        db = database.DB()
+
+        eachday_urlbases = self.listpage.get_eachday_urlbases()
+
+        for urlbase in eachday_urlbases:
+            prev_page = 0
+            now_page = 1
+
+            test_breaker = 0
+
+            while prev_page != now_page and test_breaker < 1:
+                response = self.get_request(urlbase + str(now_page), header)
+                print('\nlisturl: ' + urlbase + str(now_page) + '\n')
+
+                newslist_html = response.text
+                newslist_soup = bs(newslist_html, 'html.parser')
+
+                now_page = self.listpage.get_nowpage(newslist_soup)
+                # 일단 마음에 안 들지만 이렇게 해 두었습니다.
+                if(now_page == prev_page):
+                    break
+
+                news_urls= self.listpage.get_news_urls(newslist_soup)
+
+                futures = [asyncio.ensure_future(self.get_contents(news_url, self.contentspage, db, header)) for news_url in news_urls]
+
+                await asyncio.gather(*futures)
+
+                print("nowpage: " + str(now_page) + '\n')
+                time.sleep(crawler_const.CRAWLING_INTERVAL)
+
+                test_breaker += 1
+
+                prev_page = now_page
+                now_page += 1
+
+        db.select_all()
+        db.close()
