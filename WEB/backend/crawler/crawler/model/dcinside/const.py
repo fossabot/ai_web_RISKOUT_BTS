494fc92823438189c9887d79ab201cfd8fe3c8aa
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-09-25 15:48:43 +0000

    update consts and urlinfo at site.py

---
+++ WEB/backend/crawler/crawler/model/dcinside/const.py
@@ -0,0 +1,9 @@
+# BASEURL 하드코딩
+DOMAIN = 'dcinside'
+NAVY = "navy"
+DC_BASE = f"https://gall.dcinside.com/board/lists/?id={NAVY}"
+
+DC_CUSTOM_HEADER = {
+    'referer' : "https://gall.dcinside.com/",
+    'user-agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+}
