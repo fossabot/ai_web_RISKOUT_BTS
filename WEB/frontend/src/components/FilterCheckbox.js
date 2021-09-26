aeb4551366bc327d933e6a4d6bd618ceb11f02e2
Simon Seo <simonseo.doubles@gmail.com>
2021-09-26 14:12:46 +0000

    display filters from search response

--- WEB/frontend/src/components/FilterCheckbox.js
+++ WEB/frontend/src/components/FilterCheckbox.js
@@ -1,11 +1,14 @@
 import React from "react";
 
 export default function filterCheckbox(props) {
-    const { count, hashtag, onToggleHashtag } = props;
+    const { count, hashtag, onToggle } = props;
+    const onChange = (e) => {
+        onToggle(hashtag);
+    }
     return (
         <li>
             <label>
-                <input type="checkbox" />
+                <input type="checkbox" onChange={onChange}/>
                 <p>{hashtag}</p>
             </label>
             <em>{count > 10 ? '10+' : count}</em>
--- WEB/frontend/src/pages/Secret.js
+++ WEB/frontend/src/pages/Secret.js
@@ -8,10 +8,10 @@ import searchIcon from "../images/sub/search_icon.png";
 import filtersCloseIcon from "../images/sub/filters_close.png";
 // import exampleData from "./SecretData.example.json";
 
-function Secret () {
+function Secret() {
 
-    const [ appliedFilters, setAppliedFilters ] = useState([]);
-    const [ searchResults, setSearchResults ] = useState({
+    const [appliedFilters, setAppliedFilters] = useState(['전투시행세부규칙', 'GP/GOP']);
+    const [searchResults, setSearchResults] = useState({
         "contentsLength": 0,
         "contents": [],
         "filterTags": {
@@ -20,23 +20,29 @@ function Secret () {
             "TIM": {}
         }
     });
-    useEffect(() => {
-        search();
-    }, []);
 
 
     const toggleFilter = hashtag => {
         console.log(`toggle ${hashtag}`);
+        if (appliedFilters.includes(hashtag)) {
+            setAppliedFilters(appliedFilters.filter(val => val != hashtag));
+        }
+        else {
+            appliedFilters.push(hashtag);
+            setAppliedFilters(appliedFilters);
+        }
+        search();
     };
 
     const search = () => {
         console.log(`search options: `);
         fetch('SecretData.example.json').then(res => res.json()).then((data) => {
             console.log(data);
-            console.log(setSearchResults);
             setSearchResults(data);
         });
     };
+
+    useEffect(search, [appliedFilters]);
 
     return (
         <section id="sub_contents2">
@@ -54,9 +60,12 @@ function Secret () {
 
                     <h3>20 결과, 3 필터 적용중</h3>
                     <ul className="filter_keyword clfix">
-                        <AppliedFilter hashtag="전추세부시행규칙" onRemoveHashtag={toggleFilter} key="전추세부시행규칙" />
+                        {
+                            appliedFilters.map((filter, i) => <AppliedFilter hashtag={filter} onRemoveHashtag={toggleFilter} key={filter + i} />)
+                        }
+                        {/* <AppliedFilter hashtag="전추세부시행규칙" onRemoveHashtag={toggleFilter} key="전추세부시행규칙" />
                         <AppliedFilter hashtag="KJCCS" onRemoveHashtag={toggleFilter} key="KJCCS" />
-                        <AppliedFilter hashtag="GP/GOP" onRemoveHashtag={toggleFilter} key="GP/GOP" />
+                        <AppliedFilter hashtag="GP/GOP" onRemoveHashtag={toggleFilter} key="GP/GOP" /> */}
                     </ul>
 
 
@@ -77,16 +86,17 @@ function Secret () {
                         </thead>
                         <tbody>
                             {
-                                searchResults.contents.map((val, i) => <TableRow title={val.title} />)
+                                searchResults.contents.map((val, i) =>
+                                    <TableRow
+                                        title={val.title}
+                                        preview={val.summarized}
+                                    />)
                             }
                             <TableRow
                                 title="해군 참모총장 만난 썰 푼다"
                                 preview="나 사이버 작전센터에서 근무하는데 갑자기 참모총장이 와서 나랑 악수하는거임... 사진도 하나 찍혔어. 대박이더라. ㄷㄷ 그리고 끝나니까 무슨 동전같이 생긴거 받았는데 이게 참모총장?"
                                 author="김남춘123"
                             />
-                            <TableRow />
-                            <TableRow />
-                            <TableRow />
                         </tbody>
                     </table>
                 </div>
@@ -97,37 +107,39 @@ function Secret () {
                 <div className="sub02_filter">
                     <h4>Filters</h4>
                     <button><img src={filtersCloseIcon} alt="" className="close_btn" /></button>
+
+
                     <div className="filter_con">
-                        <h5>글에서 찾은 용어</h5>
-                        <span>9</span>
+                        <h5>글에서 찾은 단체</h5>
+                        <span>{Object.entries(searchResults.filterTags.ORG).length}</span>
                         <ul className="keyword">
                             {
-                                Array.apply(0, Array(10)).map(function (x, i) {
-                                    return <FilterCheckbox count={i + 5} hashtag="전투세부시행규치" key={i} onToggleHashtag={toggleFilter} />;
+                                Object.entries(searchResults.filterTags.ORG).map(function ([hashtag, freq], i) {
+                                    return <FilterCheckbox count={freq} hashtag={hashtag} key={hashtag} onToggle={toggleFilter} />;
                                 })
                             }
                         </ul>
                         <button className="more_btn">더보기</button>
                     </div>
                     <div className="filter_con">
-                        <h5>글에서 찾은 용어</h5>
-                        <span>9</span>
+                        <h5>글에서 찾은 인물</h5>
+                        <span>{Object.entries(searchResults.filterTags.CVL).length}</span>
                         <ul className="keyword">
                             {
-                                Array.apply(0, Array(10)).map(function (x, i) {
-                                    return <FilterCheckbox count={i + 5} hashtag="전투세부시행규치" key={i} onToggleHashtag={toggleFilter} />;
+                                Object.entries(searchResults.filterTags.CVL).map(function ([hashtag, freq], i) {
+                                    return <FilterCheckbox count={freq} hashtag={hashtag} key={hashtag} onToggle={toggleFilter} />;
                                 })
                             }
                         </ul>
                         <button className="more_btn">더보기</button>
                     </div>
                     <div className="filter_con">
-                        <h5>글에서 찾은 용어</h5>
-                        <span>9</span>
+                        <h5>글에서 찾은 시간대</h5>
+                        <span>{Object.entries(searchResults.filterTags.TIM).length}</span>
                         <ul className="keyword">
                             {
-                                Array.apply(0, Array(10)).map(function (x, i) {
-                                    return <FilterCheckbox count={i + 5} hashtag="전투세부시행규치" key={i} onToggleHashtag={toggleFilter} />;
+                                Object.entries(searchResults.filterTags.TIM).map(function ([hashtag, freq], i) {
+                                    return <FilterCheckbox count={freq} hashtag={hashtag} key={hashtag} onToggle={toggleFilter} />;
                                 })
                             }
                         </ul>
