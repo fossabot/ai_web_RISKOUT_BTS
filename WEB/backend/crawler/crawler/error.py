520beeffa9825685d3b4beed9f8038bfaa37b810
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-09-28 03:55:24 +0000

    change error model

--- WEB/backend/crawler/crawler/error.py
+++ WEB/backend/crawler/crawler/error.py
@@ -1,13 +1,6 @@
-class PagingTagNotFoundError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return self.msg
-
-class PageDivNotFoundError(Exception):
-    def __init__(self, msg):
-        self.msg = msg
+class HTMLElementsNotFoundError(Exception):
+    def __init__(self, location, cant_find_what):
+        self.msg = "in " + location + "...HTMLElementsNotFoundError: " + "can't find " + cant_find_what
 
     def __str__(self):
         return self.msg
