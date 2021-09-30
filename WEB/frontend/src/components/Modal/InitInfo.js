d3a0aa06624b07aad7009e844e32138d83834be4
Faper <43310141+Seo-Faper@users.noreply.github.com>
2021-09-30 11:03:38 +0000

    ui 수정

--- WEB/frontend/src/components/Modal/InitInfo.js
+++ WEB/frontend/src/components/Modal/InitInfo.js
@@ -14,11 +14,6 @@ const handleEmailChange = (e) => {
     return (
         <div className="login-container">
         <div className="login-box">
-          <div className="exit">
-              <button onClick={()=>{ history.goBack()}}>
-                <svg stroke="currentColor" fill="currentColor" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path></svg>
-              </button>
-          </div>
           <span>비밀번호 찾기</span>
               <center>
             <input type="text" placeholder="이메일을 입력하세요." onChange={handleEmailChange}/>
