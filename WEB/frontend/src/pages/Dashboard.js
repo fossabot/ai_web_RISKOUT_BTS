5f6aa4acfdc861bdb212e01b7466fbb03c5e4680
PFF <50140505+playff@users.noreply.github.com>
2021-10-01 09:47:09 +0000

    Add color palette candidate

--- WEB/frontend/src/pages/Dashboard.js
+++ WEB/frontend/src/pages/Dashboard.js
@@ -14,7 +14,12 @@ import { words } from '../dummy/words';
 
 const options = {
   // colors 를 바꾸면 전체 Theme 이 바뀝니다.
-  colors: ['#EEEEEE', '#686D76', '#373A40', '#00ADB5'],
+  colors: ['#f4a522', '#6092cd', '#61b546', '#aa4498', '#dccc77', '#89cdf0'],
+  // 1. ['#EEEEEE', '#686D76', '#373A40', '#00ADB5'],
+  // 2. ['#003f5c', '#444e86', '#955196', '#dd5182', '#ff6e54', '#ffa600'],
+  // 3. ['#01b8aa', '#28383c', '#fd625e', '#f2c80f', '#5f6b6d', '#8ad4eb'],
+  // 4. ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b'],
+  // 5. ['#f4a522', '#6092cd', '#61b546', '#aa4498', '#dccc77', '#89cdf0'],
   enableTooltip: true,
   deterministic: true,
   fontFamily: 'impact',
