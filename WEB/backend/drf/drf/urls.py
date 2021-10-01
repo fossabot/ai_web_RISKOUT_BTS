09f68c08625391bc1c3d601a1a690579bd5bd482
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-01 23:04:05 +0000

    add react path to django urls

--- WEB/backend/drf/drf/urls.py
+++ WEB/backend/drf/drf/urls.py
@@ -14,10 +14,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, re_path, include
+from django.shortcuts import render
+
+
+def render_react(request):
+    return render(request, "index.html")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('', include('accounts.urls')),
+    re_path(r"^$", render_react),
+    re_path(r"^(?:.*)/?$", render_react),
 ]
