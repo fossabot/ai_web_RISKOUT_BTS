6dcbe0b1e396c94e709f8c99b73c49d75b39f263
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-02 00:34:34 +0000

    integration frontend and backend

--- WEB/backend/drf/init_superuser.py
+++ WEB/backend/drf/init_superuser.py
@@ -1,12 +1,20 @@
 import os
+import django;
+from django.contrib.auth.management.commands.createsuperuser import get_user_model;
+from django.db.utils import IntegrityError
+
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "drf.settings");
+
 DJANGO_DB_NAME = os.environ['DJANGO_DB_NAME']
 DJANGO_SU_NAME = os.environ['DJANGO_SU_NAME']
 DJANGO_SU_EMAIL = os.environ['DJANGO_SU_EMAIL']
 DJANGO_SU_PASSWORD = os.environ['DJANGO_SU_PASSWORD']
 
-import django;
 django.setup();
 
-from django.contrib.auth.management.commands.createsuperuser import get_user_model;
-get_user_model()._default_manager.db_manager(DJANGO_DB_NAME).create_superuser(username=DJANGO_SU_NAME,email=DJANGO_SU_EMAIL,password=DJANGO_SU_PASSWORD)
+try:
+    get_user_model()._default_manager.db_manager(DJANGO_DB_NAME).create_superuser(username=DJANGO_SU_NAME,email=DJANGO_SU_EMAIL,password=DJANGO_SU_PASSWORD)
+except IntegrityError:
+    pass
+
--- WEB/backend/web.sh
+++ WEB/backend/web.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-sudo docker-compose -f web-docker-compose.yml --env-file web-docker-env build --no-cache
-sudo docker-compose -f web-docker-compose.yml --env-file web-docker-env up
+cd ../frontend
+
+npm i
+npm run build
+
+rm -rf ../backend/drf/build/*
+cp -r ./build ../backend/drf/
+
+cd ../backend
+
+sudo docker-compose -f web-docker-compose.yml --env-file web-docker-env up --force-recreate
