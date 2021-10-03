ca4c678ac9e9817b8f6bc62118430063a80882cd
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-03 00:13:16 +0000

    separate mongo DB Handler host and port

--- WEB/backend/analyzer/analyzer.py
+++ WEB/backend/analyzer/analyzer.py
@@ -92,7 +92,7 @@ class Content:
 
 class DBHandler:
     def __init__(self):
-        host = "host.docker.internal:8001"
+        host = "host.docker.internal"
         port = "8001"
         self.client = MongoClient(host, int(port))
 
