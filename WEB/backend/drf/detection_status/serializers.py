2c7224aac9457b47bfdaafd1d6da825e914c03aa
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-03 01:55:27 +0000

    init Analyzed Data Serializer

--- WEB/backend/drf/detection_status/serializers.py
+++ WEB/backend/drf/detection_status/serializers.py
@@ -1,4 +1,9 @@
 from rest_framework import generics, permissions
 from rest_framework import serializers
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 
+# Analyzed Data Serializer
+class AnalyzedDataSerializer(serializers.Serializer):
+
+    cateogry = serializers.CharField(required=True)
+    period = serializers.IntegerField(required=True)
