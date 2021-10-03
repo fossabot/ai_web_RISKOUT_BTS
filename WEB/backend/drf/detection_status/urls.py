7862a506f7a8b73a4b09b0fcd97faf3053b53e17
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-03 04:08:39 +0000

    change path name
    * typo fix

--- WEB/backend/drf/detection_status/urls.py
+++ WEB/backend/drf/detection_status/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import AnalyzedDataView
 
 urlpatterns = [
-    path('api/analyzed/', AnalyzedDataView.as_view(), name='register'),
+    path('api/analyzed/', AnalyzedDataView.as_view(), name='Analyzed Data'),
 ]
