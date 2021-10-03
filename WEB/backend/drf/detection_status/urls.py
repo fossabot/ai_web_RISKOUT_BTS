d13921991d06ede47b3691934ae6df753594ff52
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-03 02:15:42 +0000

    init detection_status urls

--- WEB/backend/drf/detection_status/urls.py
+++ WEB/backend/drf/detection_status/urls.py
@@ -1,4 +1,6 @@
-from django.urls import path, include
+from django.urls import path
+from .views import AnalyzedDataView
 
 urlpatterns = [
+    path('api/analyzed/', AnalyzedDataView.as_view(), name='register'),
 ]
