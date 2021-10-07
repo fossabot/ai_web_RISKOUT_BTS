f0b38415428940a43a93ed3528a3dde589212da4
Simon Seo <simonseo.doubles@gmail.com>
2021-10-07 14:37:27 +0000

    add link to entire card

--- WEB/frontend/src/components/ThreatMediaCard.js
+++ WEB/frontend/src/components/ThreatMediaCard.js
@@ -20,45 +20,47 @@ export default function ThreatMediaCard(props) {
   } = props;
   return (
     <Card variant="outlined" sx="width: 400px">
-      <CardMedia component="img" height="150" image={imageUrl} />
-      <CardContent>
-        <Typography
-          variant="h3"
-          sx={{
-            mt: '1em',
-            mb: '1em',
-            lineHeight: '1.67',
-            display: '-webkit-box',
-            WebkitLineClamp: '2',
-            overflow: 'hidden',
-            textOverflow: 'ellipsis',
-            WebkitBoxOrient: 'vertical',
-            // -webkit-box-orient: vertical;
-          }}
-        >
-          {title}
-        </Typography>
-        <Grid
-          container
-          direction="row"
-          justifyContent="space-between"
-          alignItems="center"
-          sx={{ mt: '1rem' }}
-        >
-          <Stack direction="row" spacing={1}>
-            <Chip
-              label={threatType}
-              // variant="outlined"
-              color="error"
-              size="medium"
-              sx={{ height: '2.4rem', fontSize: '1rem' }}
-            />
-          </Stack>
-          <Link href={href} target="_blank" rel="noopener" underline="hover">
-            {sourceChannel} {sourceTime}
-          </Link>
-        </Grid>
-      </CardContent>
+      <a href={href} style={{ padding: 0, margin: 0 }}>
+        <CardMedia component="img" height="150" image={imageUrl} />
+        <CardContent>
+          <Typography
+            variant="h3"
+            sx={{
+              mt: '1em',
+              mb: '1em',
+              lineHeight: '1.67',
+              display: '-webkit-box',
+              WebkitLineClamp: '2',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis',
+              WebkitBoxOrient: 'vertical',
+              // -webkit-box-orient: vertical;
+            }}
+          >
+            {title}
+          </Typography>
+          <Grid
+            container
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ mt: '1rem' }}
+          >
+            <Stack direction="row" spacing={1}>
+              <Chip
+                label={threatType}
+                // variant="outlined"
+                color="error"
+                size="medium"
+                sx={{ height: '2.4rem', fontSize: '1rem' }}
+              />
+            </Stack>
+            <Link href={href} target="_blank" rel="noopener" underline="hover">
+              {sourceChannel} {sourceTime}
+            </Link>
+          </Grid>
+        </CardContent>
+      </a>
     </Card>
   );
 }
