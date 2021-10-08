2251555c5dec6aeef452f957b6e92cb5c8177fbd
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-10-08 01:51:16 +0000

    add twitter logic

--- WEB/backend/crawler/crawler/model/siteInstanceServer.py
+++ WEB/backend/crawler/crawler/model/siteInstanceServer.py
@@ -1,10 +1,12 @@
 from crawler.model.naver.NaverNewsSite import NaverNewsSite
 from crawler.model.dcinside.DCSite import DCSite
+from crawler.model.twitter.Twitter import Twitter
 
 def get_siteInstance_list():
     ret = {
         'naver_news': NaverNewsSite(),
-        'dcinside': DCSite()
+        'dcinside': DCSite(),
+        'twitter': Twitter()
     }
 
     return ret
