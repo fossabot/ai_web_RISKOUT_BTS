f1ef8b6b32aa23cfb086a4ea414e7418c286e4cc
PFF <50140505+playff@users.noreply.github.com>
2021-10-10 13:53:33 +0000

    Fix Search Components

--- WEB/frontend/src/components/Search.js
+++ WEB/frontend/src/components/Search.js
@@ -1,30 +1,28 @@
-import React, {useState} from 'react';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import AutocompleteInSearch from './AutocompleteInSearch';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
-import {sampleData, sampleOptions} from './sample.js';
-// import "../App.css";
-
+import { sampleData, sampleOptions } from './sample.js';
 
 export default function Search() {
   const onSubmit = (e) => {
     const textValue = e.target.value;
     console.log(textValue);
   };
-  
+
   return (
     <Box>
-      <form onSubmit={onSubmit} style={{display: 'flex'}}>
+      <form onSubmit={onSubmit} style={{ display: 'flex' }}>
         <AutocompleteInSearch tableData={sampleData} options={sampleOptions} />
         <Button
-          sx={{ width: "", height: '5.1em' }}
+          sx={{ width: '', height: '5.1em' }}
           variant="contained"
           type="submit"
-        ><SearchIcon /></Button>
+        >
+          <SearchIcon />
+        </Button>
       </form>
     </Box>
   );
 }
-
-
