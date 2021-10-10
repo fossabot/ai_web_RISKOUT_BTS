78bfbcd319ef10c972f87b7c13fd385f46171a25
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-10 10:52:00 +0000

    * add SentimentPieDataView

--- WEB/backend/drf/detection_status/urls.py
+++ WEB/backend/drf/detection_status/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('wordcloud/', WordcloudDataView.as_view(), name='wordcloud'),
     path('article/volume/', ArticleVolumeDataView.as_view(), name='article_volume'),
     path('sentiment/bar/', SentimentBarDataView.as_view(), name='sentiment_bar'),
-    path('sentiment/pie/', AnalyzedDataView.as_view(), name='sentiment_pie'),
+    path('sentiment/pie/', SentimentPieDataView.as_view(), name='sentiment_pie'),
 
 ]
