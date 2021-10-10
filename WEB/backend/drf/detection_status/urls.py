acf9fe114483ec910cf7c8c729ae68b799973eb5
dev-taewon-kim <85913822+dev-taewon-kim@users.noreply.github.com>
2021-10-10 06:17:27 +0000

    * add urls

--- WEB/backend/drf/detection_status/urls.py
+++ WEB/backend/drf/detection_status/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import AnalyzedDataView
+from .views import *
 
 urlpatterns = [
     path('analyze/', AnalyzedDataView.as_view(), name='analyze'),
+    path('trends/', TrendsDataView.as_view(), name='trends'),
+    path('wordcloud/', AnalyzedDataView.as_view(), name='wordcloud'),
+    path('article/volume/', AnalyzedDataView.as_view(), name='true_counter'),
+    path('sentiment/bar/', AnalyzedDataView.as_view(), name='sentiment_bar'),
+    path('sentiment/pie/', AnalyzedDataView.as_view(), name='sentiment_pie'),
+
 ]
