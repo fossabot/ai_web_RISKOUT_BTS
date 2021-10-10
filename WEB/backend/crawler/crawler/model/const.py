6be8cd1791c3805891f20dec3e52bd61f26012e9
Yongjun Park <50614833+flydog98@users.noreply.github.com>
2021-10-10 07:28:45 +0000

    delete unnecessary consts

--- WEB/backend/crawler/crawler/model/const.py
+++ WEB/backend/crawler/crawler/model/const.py
@@ -1,12 +1,5 @@
-
 # 리스트 페이지마다 크롤링 간격 (초)
-CRAWLING_LIST_INTERVAL = 30
-
-# 페이지와 페이지 사이마다 크롤링 간격 (초)
-CRAWLING_PAGE_INTERVAL = 5
+CRAWLING_LIST_INTERVAL = 5
 
 # 크롤링할 날짜 수
-CRAWL_DATEAMOUNT = 30
-
-# 테스트용으로 크롤링할 페이지 수 제한
-MAX_LISTPAGE_CRAWL = 10
+CRAWL_DATEAMOUNT = 3
